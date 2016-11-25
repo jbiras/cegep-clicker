4c13674b917555784584aa7670d5df0b0181d967
jbiras <saltysnapland@gmail.com>
2016-11-25 17:13:02 -0500

    Commit initial
    Création du projet phonegap (avec les bons fichiers)

---
+++ app/src/main/java/mobile/informatique/cgmatane/qc/ca/cegepclicker/CegepClicker.java
@@ -0,0 +1,18 @@
+package mobile.informatique.cgmatane.qc.ca.cegepclicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.apache.cordova.*;
+import org.apache.cordova.DroidGap;
+
+
+public class CegepClicker extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.init();
+        super.loadUrl(Config.getStartUrl());
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
